e2144da431b43298e76c17f6bb98911eebf1c424
Kriskitt <152028480+Kriskitt@users.noreply.github.com>
2024-12-17 09:25:51 -0500

    Delete ml_logic directory

--- ml_logic/params.py
+++
@@ -1,2 +0,0 @@
-import os
-MODEL_PATH=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'model')
--- ml_logic/preprocess.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def preprocess_image(img_path):
-    image = cv2.imread(img_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (224, 224))
-    image = image / 255.0  # Normalizar
-    return image
